e967b4c9764dc05d6f7599de128f95b91572f7c6
thalesmenegueco <thales.menegueco@gmail.com>
2025-03-26 11:41:54 -0300

    first step with php

---
+++ 1_codigo.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta</title>
+</head>
+    <body>
+        <?php echo "olá ".$_POST["name"] ?>
+        <br>
+        <?php echo "Sua senha é ".$_POST["psswrd"] ?>
+    </body>
+</html>
---
+++ 1_index.php
@@ -0,0 +1,19 @@
+<php>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cadastro simples</title>
+        </head>
+        <body>
+        <form action="1_codigo.php" method="POST">
+            <label for="name">Name:</label>
+            <input type="text" name="name"><br>
+            <label for="psswrd">Senha:</label>
+            <input type="password" name="psswrd"><br>
+            <input type="submit" value="Entrar">
+        </form>
+        </body>
+    </html>
+</php>
